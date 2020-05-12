37b93d05bb50d5e61b487e048a55a0ff2ad2e89f
jkk004 <62976976+jkk004@users.noreply.github.com>
2020-05-12 02:00:31 -0700

    Update createvisual.py

--- createvisual.py
+++ createvisual.py
@@ -1,14 +1,18 @@
+  
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 #copy and paste the data from main.py here, or you could always put the code from here to the main.py file. I just made it here to make the code simpler
 plt.style.use("ggplot")
-rankings = [3, 119, 184, 76, 19, 58, 22, 52, 13, 8, 80, 14, 124, 41, 39, 2, 10, 184, 65]
-front = ['David Toms', 'Bernhard Langer', 'Brian Henninger', 'Brad Faxon', 'Ben Crane', 'Ben Crane', 'Tim Clark', 'Daniel Chopra', 'Brian Gay', 'Paul Casey', 'Charlie Wi', 'Luke Donald', 'Greg Chalmers', 'Graeme McDowell', 'Russell Henley', 'Jason Day', 'Brian Harman', 'Greg Chalmers', 'Graeme McDowell']
-tot=['David Toms 2001', 'Bernhard Langer 2002', 'Brian Henninger 2003', 'Brad Faxon 2004', 'Ben Crane 2005', 'Ben Crane 2006', 'Tim Clark 2007', 'Daniel Chopra 2008', 'Brian Gay 2009', 'Paul Casey 2010', 'Charlie Wi 2011', 'Luke Donald 2012', 'Greg Chalmers 2013', 'Graeme McDowell 2014', 'Russell Henley 2015', 'Jason Day 2016', 'Brian Harman 2017', 'Greg Chalmers 2018', 'Graeme McDowell 2019']
+rankings = [61, 112, 250, 99, 170, 90, 55, 58, 127, 51, 62, 5, 173, 2, 5, 27, 39, 14, 64]
+front = ['John Daly', 'John Daly', 'Hank Kuehne', 'Hank Kuehne', 'Scott Hend', 'Bubba Watson', 'Bubba Watson', 'Bubba Watson', 'Robert Garrigus', 'Robert Garrigus', 'J.B. Holmes', 'Bubba Watson', 'Luke List', 'Bubba Watson', 'Dustin Johnson', 'J.B. Holmes', 'Rory McIlroy', 'Rory McIlroy', 'Cameron Champ']
+years = ['2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+tot = []
+for i in range(19):
+    tot.append(front[i] + " " + years[i])
 
 plt.barh(tot, rankings,color = 'k')
 plt.xlabel("Money Leader Ranking (Lower The Better)")
-plt.title("Best Putter of the Year vs Actual Ranking")
+plt.title("Best Driver of the Year vs Actual Ranking")
 
 plt.show()
